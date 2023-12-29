c4fd4bed12269a0cd3a784ede880e9bb29ecf1b6
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2023-12-29 17:15:45 -0300

    enhancement: overhauled the options handler

--- ControlFlowGraphs/driver.py
+++ ControlFlowGraphs/driver.py
@@ -3,12 +3,13 @@ from todo import *
 
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
+
+    options_map = {
+        "test_min": test_min(int(lines[1]), int(lines[2])),
+        "test_min3": test_min3(int(lines[1]), int(lines[2]), int(lines[3])),
+        "test_div": test_div(int(lines[1]), int(lines[2])),
+        "fact": test_fact(int(lines[1]))
+    }
     option = lines[0].strip()
-    if option == "test_min":
-        print(test_min(int(lines[1]), int(lines[2])))
-    if option == "test_min3":
-        print(test_min3(int(lines[1]), int(lines[2]), int(lines[3])))
-    if option == "test_div":
-        print(test_div(int(lines[1]), int(lines[2])))
-    if option == "fact":
-        print(test_fact(int(lines[1])))
+
+    print(options_map.get(option))
