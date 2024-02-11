df89e1a7dc6a587bccbada9f087f0922b52c230f
Fernando Quintao Pereira <pronesto@gmail.com>
2024-02-11 09:08:20 -0300

    Adding changes to driver.

--- ControlFlowGraphs/driver.py
+++ ControlFlowGraphs/driver.py
@@ -3,13 +3,14 @@ from todo import *
 
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
-
-    options_map = {
-        "test_min": test_min(int(lines[1]), int(lines[2])),
-        "test_min3": test_min3(int(lines[1]), int(lines[2]), int(lines[3])),
-        "test_div": test_div(int(lines[1]), int(lines[2])),
-        "fact": test_fact(int(lines[1]))
-    }
     option = lines[0].strip()
-
-    print(options_map.get(option))
+    if option == "test_min":
+        print(test_min(int(lines[1]), int(lines[2])))
+    elif option == "test_min3":
+        print(test_min3(int(lines[1]), int(lines[2]), int(lines[3])))
+    elif option == "test_div":
+        print(test_div(int(lines[1]), int(lines[2])))
+    elif option == "fact":
+        print(test_fact(int(lines[1])))
+    else:
+        print("Invalid option: {option}")
