dae33c71d6cce4c4c7b15e6eb3a231ae2ff91510
Henrique Furst Scheid <hfscheid@gmail.com>
2024-06-11 21:57:14 -0300

    feat: implemented type checking

---
+++ TypeChecking/driver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import lang
+import parser
+
+if __name__ == "__main__":
+    lang.Inst.next_index = 0
+    lines = sys.stdin.readlines()
+    env, program = parser.file2cfg_and_env(lines)
+    try:
+        lang.type_check(program[0], lang.TypeEnv.from_env(env))
+    except lang.InstTypeErr as e:
+        print(e)
