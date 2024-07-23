e015a21526a0d578bb38b856036d075de7a6508c
Henrique Furst Scheid <hfscheid@gmail.com>
2024-07-22 21:03:49 -0300

    feat: added ssa fibonnacci and big_branch test cases for ssa register allocation

--- SSARegisterAllocation/driver.py
+++ SSARegisterAllocation/driver.py
@@ -46,3 +46,39 @@ def ssa_fibonacci() -> list[Inst]:
     for i in range(12):
         prog[i].add_next(prog[i+1])
     return prog, env
+
+
+def ssa_big_branch() -> list[Inst]:
+    env = Env()
+    i0 = Add('sub', 3, -2)
+    i1 = Lth('pred', 'sub', 1)
+    i3 = Add('prod0', 0, 1)
+    i4 = Add('count0', 0, 1)
+    i5 = Phi('prod1', 'prod0', 'prod2')
+    i6 = Phi('count1', 'count0', 'count2')
+    i7 = Mul('prod2', 'prod1', 2)
+    i8 = Add('count2', 'count1', 1)
+    i9 = Geq('repeat0', 3, 'count2')
+    i10 = Bt('repeat0', i5)
+    i12 = Add('fib0', 0, 1)
+    i13 = Add('count3', 0, 1)
+    i14 = Phi('count4', 'count3', 'count5')
+    i15 = Phi('fib1', 'fib0', 'fib2')
+    i16 = Add('nextfib', 'fib1', 1)
+    i17 = Add('fib2', 'fib1', 'nextfib')
+    i18 = Add('count5', 'count4', 1)
+    i19 = Geq('repeat1', 3, 'count5')
+    i20 = Bt('repeat1', i13)
+    i21 = Add('end', 0, 0)
+    i11 = Bt(True, i19)
+    i2 = Bt('pred', i11)
+    prog = [
+        i0, i1, i2, i3, i4,
+        i5, i6, i7, i8, i9,
+        i10, i11, i12, i13, i14,
+        i15, i16, i17, i18, i19,
+        i20, i21
+    ]
+    for i in range(20):
+        prog[i].add_next(prog[i+1])
+    return prog, env
